88592896e65dd752fa9466bb83a3282aae5a6b5d
mp768 <60555816+mp768@users.noreply.github.com>
2024-01-30 21:43:09 -0600

    Got more progress done.
    I'm currently setting up all the basic stuff to get files loaded and
    starting to parse the comments. I'm also looking to parse the beginning
    line of every handwritten file to match it with it's proper class.
    
    I'm thinking of either using a relative path to a file to link it or
    using the class name. Probably for convenience using a class name will
    be the simplest for implementation and for users.
    
    After I get all the markdown to generate, I need to figure out how I
    should go about transforming it to HTML. I could probably use a library
    to do it, but I could leave the option up to the user. This would allow
    them to either use a static standard Markdown to HTML generator or use
    their own Team custom page layout.

--- generate_docs.py
+++ generate_docs.py
@@ -1,27 +1,74 @@
 import os
 import glob
+import argparse
+import json
 
 # The path to start pulling ".java" files from.
-relative_path_for_lib: str = None
+lib_folder_path: str = None
 
 # The path for where to generate the resulting ".md" markdown files.
-output_path: str = None
+markdown_output_path: str = None
 
 # The path that stores all the files for handwritten documentation.
-path_for_handwritten_docs: str = None
+handwritten_docs_folder_path: str = None
 
-# Config file
-config_file_location = "config.txt"
+# Config file (default name and location)
+config_file_location = "generate_config.json"
 
+# Json file contents that are required to make this project run.
 DEFAULT_CONFIG_INFO: str = """
-    relative_path_for_lib: "." # Your relative path
-    output_path: "." # Output path for markdown files
-    path_for_handwritten_docs: "." # Path for handwritten docs.
+{
+    "lib_folder_path":              ".",
+    "markdown_output_path":         "out",
+    "handwritten_docs_folder_path": "."
+}
 """;    
 
 if not os.path.exists(config_file_location):
+    print("Creating New Config File...")
     with open(config_file_location, "w") as f:
-        f.write(DEFAULT_CONFIG_INFO.strip().replace("    ", ""))
+        f.write(DEFAULT_CONFIG_INFO.strip())
+
+# Quick little argument parser
+argument_parser = argparse.ArgumentParser(description="A python script to parse java files in team 1816's lib folder.")
+
+argument_parser.add_argument("-cf", "--config_file", required=False, help="The path for a configuration json file.")
+argument_parser.add_argument("--generate_default_config", required=False, action="store_true", help="A little helper to quickly generate a default config file. Does not run the rest of the program.")
+
+args = argument_parser.parse_args()
+
+if args.generate_default_config is not None and args.generate_default_config:
+    print("Creating Default Config File...")
+    with open("default_config.json", "w") as f:
+        f.write(DEFAULT_CONFIG_INFO.strip())
+    
+    exit()
+
+if args.config_file is not None:
+    config_file_location = args.config_file
+
+# Get all the config data out of our json file.
+try:
+    with open(config_file_location) as f:
+        config = json.load(f)
+        
+        lib_folder_path = config['lib_folder_path']
+        markdown_output_path = config['markdown_output_path']
+        handwritten_docs_folder_path = config['handwritten_docs_folder_path']
+        
+        print("this is what config contains:", config)
+except:
+    print(f"Unable to read configuration file at \"{config_file_location}\". \nIt could've been a file type that wasn't json, wasn't correctly configured, or doesn't exist.")
+    exit()
+
+if not os.path.exists(markdown_output_path):
+    os.makedirs(markdown_output_path)
+
+# Variables to store what files to process
+# file_path -> file object
+
+java_files: map = {}
+handwritten_files: map = {}
 
 
 
