4fc13d733754589262f991253300cdec2d99f0e7
mp768 <60555816+mp768@users.noreply.github.com>
2024-01-30 20:12:05 -0600

    Started some more work on the project.

--- generate_docs.py
+++ generate_docs.py
@@ -1,3 +1,27 @@
 import os
 import glob
-import args
+
+# The path to start pulling ".java" files from.
+relative_path_for_lib: str = None
+
+# The path for where to generate the resulting ".md" markdown files.
+output_path: str = None
+
+# The path that stores all the files for handwritten documentation.
+path_for_handwritten_docs: str = None
+
+# Config file
+config_file_location = "config.txt"
+
+DEFAULT_CONFIG_INFO: str = """
+    relative_path_for_lib: "." # Your relative path
+    output_path: "." # Output path for markdown files
+    path_for_handwritten_docs: "." # Path for handwritten docs.
+""";    
+
+if not os.path.exists(config_file_location):
+    with open(config_file_location, "w") as f:
+        f.write(DEFAULT_CONFIG_INFO.strip().replace("    ", ""))
+
+
+
